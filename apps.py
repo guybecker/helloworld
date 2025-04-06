6dbd085ef216a8c4bcb665b5c26799bb86d41f87
Guy Becker <guybecker@gmail.com>
2025-04-06 20:50:42 +0300

    Update apps.py

--- apps.py
+++ apps.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return send_from_directory('.', 'index.html')
-
+#
 # This is needed for Azure to properly start the application
 if __name__ == '__main__':
     # Azure App Service expects your app to listen on port 8000
