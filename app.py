6240a79c132ba59d8d68ae5d0f22b69080e7e86e
Guy Becker <guybecker@gmail.com>
2025-04-06 19:23:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='.')
 
 @app.route('/')
 def home():
     return send_from_directory('.', 'index.html')
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-
+# This is needed for Azure to find the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port="8400",debug=True)
+    # Use the PORT environment variable provided by Azure if available
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+else:
+    # For Gunicorn to find the app
+    application = app
