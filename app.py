8f18fe2983f8020774242aa59c23ffff3767e12e
Guy Becker <guybecker@gmail.com>
2025-04-06 19:27:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_folder='.')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return send_from_directory('.', 'index.html')
 
-# This is needed for Azure to find the app
+# This is needed for Azure to properly start the application
 if __name__ == '__main__':
-    # Use the PORT environment variable provided by Azure if available
-    port = int(os.environ.get('PORT', 5000))
+    # Azure App Service expects your app to listen on port 8000
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
-else:
-    # For Gunicorn to find the app
-    application = app
